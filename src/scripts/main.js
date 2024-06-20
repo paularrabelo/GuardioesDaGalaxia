09b454598728c2bb5794599c590af7000b5d8eca
Paula Rabelo <paula_rabelo2010@hotmail.com>
2024-06-19 22:14:35 -0300

    ajustes no menu e responsividade do projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll('[data-faq-question]');
     
     const homeSection = document.querySelector('.home');
     const alturaHome = homeSection.clientHeight;
@@ -25,30 +24,41 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('filmes__tabs__button--is-active');
         })
     }
+
 })
 
-    function ocultaElementosDoHeader () {
-        const header = document.querySelector('header');
-        header.classList.add('header--is-hidden');
-    }
+function ocultaElementosDoHeader () {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
     
-    function exibeElementosDoHeader () {
-        const header = document.querySelector('header');
-        header.classList.remove('header--is-hidden');
-    }
+function exibeElementosDoHeader () {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
-    function removeBotaoAtivo () {
-        const buttons = document.querySelectorAll('[data-tab-button]');
+function removeBotaoAtivo () {
+    const buttons = document.querySelectorAll('[data-tab-button]');
     
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].classList.remove('filmes__tabs__button--is-active');
-        }
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('filmes__tabs__button--is-active');
     }
+}
 
-    function escondeTodasAabas () {
-        const tabsContainer = document.querySelectorAll('[data-tab-id]');
+function escondeTodasAabas () {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
     
-        for (let i = 0; i < tabsContainer.length; i++) {
-            tabsContainer[i].classList.remove('filmes__list--is-active');
-        }
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('filmes__list--is-active');
     }
+}
+
+function clickMenu () {
+    const menuMobile = document.querySelector('.mobile__menu');
+
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    } else {
+        menuMobile.classList.add('open');
+    }
+}
